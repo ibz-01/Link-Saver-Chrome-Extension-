0614ccc3be1f97b8a344ffb21cb9d05025fdff9f
ibz-01 <ibrahimkhalid923@gmail.com>
2026-06-19 22:50:02 +0500

    added inputfield template

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function save()
 {
     console.log("save pressed")
 }
+
+document.getElementById("input-btn").addEventListener("click",
+    function()
+    {
+        console.log("Input field clicked")
+    }
+)
